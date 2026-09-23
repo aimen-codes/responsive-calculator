3b2de9b702c4d293a792fc0cbccac185ade5a4a1
Aimen Khan <aimenkhanishere@gmail.com>
2026-09-23 16:35:56 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let display = document.getElementById("display");
+
+
+function appendValue(value) {
+  if (value === '×') {
+    display.value += '*';
+  } else {
+    display.value += value;
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
